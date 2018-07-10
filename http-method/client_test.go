46dc83f0fd40ce61df8a8b347afc1d2dbc0040c0
guoxuemeng <89016230@qq.com>
2018-07-10 18:46:26 +0800

    http method

---
+++ http-method/client_test.go
@@ -0,0 +1,150 @@
+package main
+
+import (
+	"testing"
+	"net/http"
+	"io/ioutil"
+	"fmt"
+	"strings"
+	"net/url"
+	"bytes"
+)
+
+func TestGet(t *testing.T) {
+
+	resp, err := http.Get("http://localhost:5100/")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("TestGet", string(body))
+
+}
+
+func TestGetWithQueryString(t *testing.T) {
+
+	resp, err := http.Get("http://localhost:5100/with_query_string?id=1000&page=1")
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("TestGetWithQueryString", string(body))
+
+}
+
+func TestPost(t *testing.T) {
+
+	resp, err := http.Post("http://localhost:5100/post",
+		"application/x-www-form-urlencoded",
+		strings.NewReader("username=admin"))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("TestPost", string(body))
+}
+
+func TestPostForm(t *testing.T) {
+
+	resp, err := http.PostForm("http://localhost:5100/post",
+		url.Values{"username": {"admin"}})
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("TestPostForm", string(body))
+
+}
+
+func TestGetPost1(t *testing.T) {
+
+	var r http.Request
+	r.ParseForm()
+	r.Form.Add("username", "admin")
+
+	queryStr := strings.TrimSpace(r.Form.Encode())
+
+	request, err := http.NewRequest(http.MethodPost,
+		"http://localhost:5100/get_post",
+		strings.NewReader(queryStr))
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	request.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+
+	var resp *http.Response
+	resp, err = http.DefaultClient.Do(request)
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("TestGetPost1", string(body))
+
+}
+
+func TestPostBinary(t *testing.T) {
+
+	data := bytes.NewReader([]byte("Hello world"))
+
+	request, err := http.NewRequest(http.MethodPost, "http://localhost:5100/post_binary", data)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	var resp *http.Response
+	resp, err = http.DefaultClient.Do(request)
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fmt.Println("TestPostBinary", string(body))
+}
---
+++ http-method/server.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"net/http"
+	"encoding/json"
+	"log"
+	"io/ioutil"
+)
+
+func main() {
+
+	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println(request.Method, request.URL.Path)
+		writer.Write([]byte("Hello world"))
+	})
+
+	http.HandleFunc("/with_query_string", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println(request.Method, request.URL.Path)
+		query := request.URL.Query()
+
+		queryMap := make(map[string]interface{})
+
+		for k, v := range query {
+			queryMap[k] = v[0]
+		}
+
+		content, err := json.Marshal(queryMap)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		writer.Write(content)
+	})
+
+	http.HandleFunc("/post", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println(request.Method, request.URL.Path)
+		request.ParseForm()
+
+		formMap := make(map[string]interface{})
+
+		for k, v := range request.Form {
+			formMap[k] = v[0]
+		}
+
+		content, err := json.Marshal(formMap)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		writer.Write(content)
+	})
+
+	http.HandleFunc("/get_post", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println(request.Method, request.URL.Path)
+		request.ParseForm()
+
+		formMap := make(map[string]interface{})
+
+		for k, v := range request.Form {
+			formMap[k] = v[0]
+		}
+
+		content, err := json.Marshal(formMap)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		writer.Write(content)
+
+	})
+
+	http.HandleFunc("/post_binary", func(writer http.ResponseWriter, request *http.Request) {
+		log.Println(request.Method, request.URL.Path)
+
+		body, err := ioutil.ReadAll(request.Body)
+
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		defer request.Body.Close()
+
+		writer.Write(body)
+
+	})
+
+	http.ListenAndServe(":5100", nil)
+}
